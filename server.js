fdeccfe713c26b9878a9bd853491dd5111eb73e4
xeusteerapat <xeus085@gmail.com>
2020-03-16 17:23:43 +0700

    add GET method

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const bodyParser = require('body-parser');
 const db = require('./models');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/student', (req, res) => {
+  let newStudent = {
+    name: req.body.name,
+    year: Number(req.body.year)
+  };
+  db.student
+    .create(newStudent)
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(400).send(err));
+});
+
+app.get('/', (req, res) => {
+  db.student.findAll().then(result => res.status(200).send(result));
+});
 
 db.sequelize.sync().then(() => {
   app.listen(5000, () => {
